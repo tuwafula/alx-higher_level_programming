d100e1edb3b6e0e695ec945fa9d1c356ba71d533
tush <wafulaat@gmail.com>
2023-01-08 08:39:47 +0300

    Initial commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, new_element):
+
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = new_element
+        return my_list
+
+
