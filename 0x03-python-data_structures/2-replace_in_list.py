b2ec2be421ab79370a20ba787f6e02e7431e974f
tush <wafulaat@gmail.com>
2023-01-08 13:26:57 +0300

    correct errors

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-def replace_in_list(my_list, idx, new_element):
-
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = new_element
+        my_list[idx] = element
         return my_list
-
-
