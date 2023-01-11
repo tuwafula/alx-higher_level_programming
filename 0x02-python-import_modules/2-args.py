0af62c3fa70534e5284842bcb9f69a7b7d21d844
tush <wafulaat@gmail.com>
2023-01-11 13:52:09 +0300

    number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv[1:])
+    uinput = argv[1:]
+    print("{:d} {:s}{:s}".format(size, 
+        "arguments" if (size) != 1 else "argument", 
+        "." if (size) == 0 else ":"))
+    for idx, arg in enumerate(uinput):
+        print("{:d}: {:s}".format(idx + 1, argv))
