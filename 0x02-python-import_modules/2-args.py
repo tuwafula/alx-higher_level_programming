3624eb8c7e3601586b03277807c44f31fe1b6b9b
tush <wafulaat@gmail.com>
2023-01-11 14:01:17 +0300

    correct errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    userin = argv[1:]
-    size = len(userin)
+    uinput = argv[1:]
+    size = len(uinput)
     print("{:d} {:s}{:s}".
           format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(userin):
+                 "arguments" if (size) != 1 else "argument",
+                 "." if (size) == 0 else ":"))
+    for idx, arg in enumerate(uinput):
         print("{:d}: {:s}".format(idx + 1, arg))
